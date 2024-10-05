522d08d56295a322dd4a8c7ddeac4de119b5b14a
Bartosz Solka <bartosz.solka@blockydevs.com>
2024-10-06 01:37:23 +0200

    Added functions to payment handler

--- payment-handler/src/PaymentHandler.sol
+++ payment-handler/src/PaymentHandler.sol
@@ -3,13 +3,38 @@ pragma solidity ^0.8.13;
 
 import {AccountFactory} from "../lib/account-factory/src/AccountFactory.sol";
 
+struct CodeValue {
+    address to;
+    uint256 value;
+}
+
 contract PaymentHandler {
     AccountFactory public accountFactory;
 
     address private mockedOwner = address(0);
+    mapping (uint256 => CodeValue) public codeToValue;
+    uint256 public codeNonce = 435231;
 
     constructor(AccountFactory _accountFactory){
         accountFactory = _accountFactory;
+    }
+
+    function requestTransfer(uint256 value) public {
+        codeToValue[codeNonce] = CodeValue(msg.sender, value);
+        codeNonce++;
+    }
+
+    function readCodeToValue(uint256 code) public view returns (uint256) {
+        return codeToValue[code].value;
+    }
+
+    function fulfillCode(uint256 code) payable public {
+        CodeValue memory codeValue = codeToValue[code];
+        uint256 value = codeValue.value;
+        if (value > msg.value) {
+            revert("Insufficient value");
+        }
+        codeValue.to.call{value: value}("");
     }
 
     function forwardSend(address payable to, bytes32 toEmailHash) payable public {
