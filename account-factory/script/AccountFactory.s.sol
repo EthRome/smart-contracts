5c963188f41551ee32a9137c8651330d4c48723b
Maciek Malik <poczta@maciekmalik.pl>
2024-10-05 22:14:50 +0200

    plz work plzzzz

--- account-factory/script/AccountFactory.s.sol
+++ account-factory/script/AccountFactory.s.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.23;
 
-import {Script, console} from "forge-std/Script.sol";
+import {CommonBase} from "../lib/forge-std/src/Base.sol";
+import {Script} from "../lib/forge-std/src/Script.sol";
+import {StdChains} from "../lib/forge-std/src/StdChains.sol";
+import {StdCheatsSafe} from "../lib/forge-std/src/StdCheats.sol";
+import {StdUtils} from "../lib/forge-std/src/StdUtils.sol";
 import {AccountFactory} from "../src/AccountFactory.sol";
-import {IEntryPoint} from "../lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {IEntryPoint} from "@account-abstraction/interfaces/IEntryPoint.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
@@ -11,7 +15,7 @@ contract CounterScript is Script {
     function run() public {
         vm.startBroadcast();
         address entryPoint = vm.envAddress("ENTRY_POINT_CONTRACT_ADDRESS");
-        new AccountFactory(IEntryPoint(entryPoint));
+        new AccountFactory(msg.sender, IEntryPoint(entryPoint));
         vm.stopBroadcast();
     }
 }
--- account-factory/src/AccountFactory.sol
+++ account-factory/src/AccountFactory.sol
@@ -1,55 +1,64 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.23;
+pragma solidity ^0.8.23;
 
-import {IEntryPoint} from "../lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
-import {SimpleAccount} from "../lib/account-abstraction/contracts/samples/SimpleAccount.sol";
-import {ERC1967Proxy} from "../lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import {Create2} from "../lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/utils/Create2.sol";
+import {IEntryPoint} from "@account-abstraction/interfaces/IEntryPoint.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {LightAccount} from "../lib/light-account/src/LightAccount.sol";
+import {BaseLightAccountFactory} from "../lib/light-account/src/common/BaseLightAccountFactory.sol";
+import {LibClone} from "../lib/light-account/src/external/solady/LibClone.sol";
 
-/**
- * A sample factory contract for SimpleAccount
- * A UserOperations "initCode" holds the address of the factory, and a method call (to createAccount, in this sample factory).
- * The factory's createAccount returns the target account address even if it is already installed.
- * This way, the entryPoint.getSenderAddress() can be called either before or after the account is created.
- */
-contract AccountFactory {
-    SimpleAccount public immutable accountImplementation;
+/// @title A factory contract for LightAccount.
+/// @dev A UserOperations "initCode" holds the address of the factory, and a method call (`createAccount`). The
+/// factory's `createAccount` returns the target account address even if it is already installed. This way,
+/// `entryPoint.getSenderAddress()` can be called either before or after the account is created.
+contract AccountFactory is BaseLightAccountFactory {
+    LightAccount public immutable ACCOUNT_IMPLEMENTATION;
 
-    address private mockedOwner = address(0);
-
-    constructor(IEntryPoint _entryPoint) {
-        accountImplementation = new SimpleAccount(_entryPoint);
+    constructor(address owner, IEntryPoint entryPoint) Ownable(owner) {
+        _verifyEntryPointAddress(address(entryPoint));
+        ACCOUNT_IMPLEMENTATION = new LightAccount(entryPoint);
+        ENTRY_POINT = entryPoint;
     }
 
-    /**
-     * create an account, and return its address.
-     * returns the address even if the account is already deployed.
-     * Note that during UserOperation execution, this method is called only if the account is not deployed.
-     * This method returns an existing account address so that entryPoint.getSenderAddress() would work even after account creation
-     */
-    function createAccount(address owner, uint256 salt) public returns (SimpleAccount ret) {
-        address addr = getAddress(mockedOwner, salt);
-        uint256 codeSize = addr.code.length;
-        if (codeSize > 0) {
-            return SimpleAccount(payable(addr));
+    /// @notice Create an account, and return its address. Returns the address even if the account is already deployed.
+    /// @dev During UserOperation execution, this method is called only if the account is not deployed. This method
+    /// returns an existing account address so that entryPoint.getSenderAddress() would work even after account
+    /// creation.
+    /// @param owner The owner of the account to be created.
+    /// @param salt A salt, which can be changed to create multiple accounts with the same owner.
+    /// @return account The address of either the newly deployed account or an existing account with this owner and salt.
+    function createAccount(address owner, uint256 salt) external returns (LightAccount account) {
+        (bool alreadyDeployed, address accountAddress) =
+                            LibClone.createDeterministicERC1967(address(ACCOUNT_IMPLEMENTATION), _getCombinedSalt(owner, salt));
+
+        account = LightAccount(payable(accountAddress));
+
+        if (!alreadyDeployed) {
+            account.initialize(owner);
         }
-        ret = SimpleAccount(payable(new ERC1967Proxy{salt: bytes32(salt)}(
-            address(accountImplementation),
-            abi.encodeCall(SimpleAccount.initialize, (mockedOwner))
-        )));
     }
 
-    /**
-     * calculate the counterfactual address of this account as it would be returned by createAccount()
-     * @param salt - hash of the target user email
-     */
-    function getAddress(address owner, uint256 salt) public view returns (address) {
-        return Create2.computeAddress(bytes32(salt), keccak256(abi.encodePacked(
-            type(ERC1967Proxy).creationCode,
-            abi.encode(
-                address(accountImplementation),
-                abi.encodeCall(SimpleAccount.initialize, (mockedOwner))
-            )
-        )));
+    /// @notice Calculate the counterfactual address of this account as it would be returned by `createAccount`.
+    /// @param owner The owner of the account to be created.
+    /// @param salt A salt, which can be changed to create multiple accounts with the same owner.
+    /// @return The address of the account that would be created with `createAccount`.
+    function getAddress(address owner, uint256 salt) external view returns (address) {
+        return LibClone.predictDeterministicAddressERC1967(
+            address(ACCOUNT_IMPLEMENTATION), _getCombinedSalt(owner, salt), address(this)
+        );
+    }
+
+    /// @notice Compute the hash of the owner and salt in scratch space memory.
+    /// @dev The caller is responsible for cleaning the upper bits of the owner address parameter.
+    /// @param owner The owner of the account to be created.
+    /// @param salt A salt, which can be changed to create multiple accounts with the same owner.
+    /// @return combinedSalt The hash of the owner and salt.
+    function _getCombinedSalt(address owner, uint256 salt) internal pure returns (bytes32 combinedSalt) {
+        // Compute the hash of the owner and salt in scratch space memory.
+        assembly ("memory-safe") {
+            mstore(0x00, owner)
+            mstore(0x20, salt)
+            combinedSalt := keccak256(0x00, 0x40)
+        }
     }
 }
