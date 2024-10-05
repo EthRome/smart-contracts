4fe9fe614a52a161e40cf4e00e0796af03ea1a4e
Maciek Malik <poczta@maciekmalik.pl>
2024-10-05 17:24:31 +0200

    feat: update PaymentHandler tests
    Updated the test suite for the PaymentHandler contract. The changes include:
    - Replaced CounterTest with PaymentHandlerTest
    - Added setup function to initialize AccountFactory and PaymentHandler instances
    - Introduced a basic send test case that uses forwardSend method of the payment handler

--- payment-handler/test/PaymentHandler.t.sol
+++ payment-handler/test/PaymentHandler.t.sol
@@ -1,24 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "../src/PaymentHandler.sol";
-import {Test, console} from "forge-std/Test.sol";
+import {IEntryPoint} from "../lib/account-factory/lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {AccountFactory} from "../lib/account-factory/src/AccountFactory.sol";
+import {Test} from "../lib/forge-std/src/Test.sol";
+import {PaymentHandler} from "../src/PaymentHandler.sol";
 
-contract CounterTest is Test {
+contract PaymentHandlerTest is Test {
     PaymentHandler public paymentHandler;
-//
-//    function setUp() public {
-//        counter = new Counter();
-//        counter.setNumber(0);
-//    }
-//
-//    function test_Increment() public {
-//        counter.increment();
-//        assertEq(counter.number(), 1);
-//    }
-//
-//    function testFuzz_SetNumber(uint256 x) public {
-//        counter.setNumber(x);
-//        assertEq(counter.number(), x);
-//    }
+    address private alice = makeAddr("alice");
+
+    function setUp() public {
+        AccountFactory accountFactory = new AccountFactory(IEntryPoint(address(0)));
+        paymentHandler = new PaymentHandler(accountFactory);
+    }
+
+    function test_basic_send() public {
+        address targetAddress = alice;
+        address factoryOwner = address(0);
+        bytes32 hashedEmail = keccak256("test.email@domain.com");
+        paymentHandler.forwardSend(
+            uint256(1),
+            payable(targetAddress),
+            factoryOwner,
+            hashedEmail
+        );
+
+    }
+
 }
